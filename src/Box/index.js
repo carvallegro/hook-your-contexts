ae706df896d0677a15964dd5fcb9164a6dd79cab
Valentin <carvallegro@users.noreply.github.com>
2019-10-17 12:04:18 +1100

    useeffect example

--- src/Box/index.js
+++ src/Box/index.js
@@ -1,17 +1,32 @@
-import React, {useState} from 'react'
+import 'whatwg-fetch'
+import React, { useState, useEffect } from 'react'
 import './Box.css'
 
 const Box = () => {
-    const [darkMode, setDarkMode] = useState(false)
-    return (
-        <div
-            className={`Box ${darkMode ? 'night' : 'day'}`}
-            onClick={() => setDarkMode(!darkMode)}>
-            Good
-            <br/>
-            {darkMode ? 'Night' : 'Day'}
-        </div>
-    );
+  const [darkMode, setDarkMode] = useState(false)
+  useEffect(() => {
+    const intervalId = setInterval(
+      () =>
+        window
+          .fetch('https://yesno.wtf/api')
+          .then(response => response.json())
+          .then(yesOrNo => setDarkMode(yesOrNo.answer === 'yes')),
+      5000
+    )
+
+    return () => clearInterval(intervalId)
+  })
+
+  return (
+    <div
+      className={`Box ${darkMode ? 'night' : 'day'}`}
+      onClick={() => setDarkMode(!darkMode)}
+    >
+      Good
+      <br />
+      {darkMode ? 'Night' : 'Day'}
+    </div>
+  )
 }
 
 export default Box
